442fc2f56219da5e87f86867320cdbee7e306d4a
Akshatha H R <akshathahr2005@gmail.com>
2026-08-01 14:55:24 +0530

    Fix NLTK punkt_tab dependency

--- app.py
+++ app.py
@@ -5,6 +5,30 @@ import PyPDF2
 import pandas as pd
 from difflib import SequenceMatcher
 import nltk
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+try:
+    nltk.data.find("tokenizers/punkt_tab")
+except LookupError:
+    nltk.download("punkt_tab")
+
+try:
+    nltk.data.find("taggers/averaged_perceptron_tagger")
+except LookupError:
+    nltk.download("averaged_perceptron_tagger")
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+try:
+    nltk.data.find("taggers/averaged_perceptron_tagger_eng")
+except LookupError:
+    nltk.download("averaged_perceptron_tagger_eng")
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag, word_tokenize
